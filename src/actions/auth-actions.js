c6bf0ed14fe65140efc2f6cf340a53aaab8d50dd
slledru <slledru@msn.com>
2018-05-24 16:36:26 -0600

    login works

--- src/actions/auth-actions.js
+++ src/actions/auth-actions.js
@@ -2,8 +2,32 @@ const LOGIN_PASS = 'LOGIN_PASS'
 const LOGOUT = 'LOGOUT'
 const LOGIN_FAIL = 'LOGIN_FAIL'
 
-const loginSubmit({ username, password }) => {
-  const url = ``
+const loginSubmit = ({ username, password }) => {
+  const url = `${process.env.REACT_APP_API_URL}/login`
+
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+
+  return (dispatch) => {
+    fetch(url, opts)
+      .then((result) => result.json())
+      .then((result) => {
+        const { token, error } = result
+        if (error) {
+          dispatch({ type: LOGIN_FAIL, payload: {error} })
+        }
+        else {
+          const payload = { username, token }
+          dispatch({ type: LOGIN_PASS, payload })
+        }
+      })
+  }
 }
 
-// export
+export { LOGIN_PASS, LOGIN_FAIL, loginSubmit }
