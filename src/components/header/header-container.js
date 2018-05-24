1981ed3ee0cfdf4d51e07c242bdb76c91d00ef02
slledru <slledru@msn.com>
2018-05-24 15:52:06 -0600

    added login form

--- src/components/header/header-container.js
+++ src/components/header/header-container.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import './header-container.css'
 
 // const Header = () => (
 //   <h1>Header</h1>
 // )
 class Header extends Component {
+  onLogoutClick = (event) => {
+    event.preventDefault()
+    // this.props.logout()
+  }
   render() {
     return (
       <div className="header-container">
         <a className="title" href="#title">Stupid Todos</a>
+        { this.props.username !== null && <a href="#logout" onClick={ this.LogoutClick }>{`Logout ${this.props.username}`}</a>}
       </div>
     )
   }
 }
 
-export default Header
+const mapStateToProps = (state) => state.auth
+//const mapDispatchToProps = (dispatch) => bindActionCreators({ logout }, dispatch)
+
+export default connect(mapStateToProps)(Header)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import thunk from 'redux-thunk'
@@ -9,16 +8,18 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers/root-reducer'
 import Header from './components/header/header-container'
+import Login from './components/login/login-container'
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 
 ReactDOM.render((
-  <Provider store={ createStoreWithMiddleware }>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Header />
+        <Login />
       </div>
     </BrowserRouter>
   </Provider>
-  ), document.getElementById('root'))
+), document.getElementById('root'))
 registerServiceWorker()
